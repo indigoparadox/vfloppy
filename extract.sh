0a6b49b0a2caec174b001c6a60294cddb24660bd
indigoparadox <indigo.repo@interfinitydynamics.info>
2021-06-19 16:52:49 -0400

    Made extract script more flexible.

--- extract.sh
+++ extract.sh
@@ -1,8 +1,35 @@
 #!/bin/sh
 
-IMAGE_FILE="$1"
-EXTRACT_DIR="$2"
+IMAGE_FILES=""
+EXTRACT_DIR=""
+EXT=".img"
+INDIVIDUAL=0
 
-mkdir -p "$EXTRACT_DIR"
-mcopy -si "$IMAGE_FILE" ::\* "$EXTRACT_DIR"
+while [ "$1" ]; do
+   case "$1" in
+      "-i"|"--individual")
+         INDIVIDUAL=1
+         ;;
+
+      "-e"|"--extension")
+         shift
+         EXT="$1"
+         ;;
+
+      *)
+         if [ -z "$EXTRACT_DIR" ]; then
+            EXTRACT_DIR="$1"
+            mkdir -p -v "$EXTRACT_DIR"
+         elif [ $INDIVIDUAL -eq 1 ]; then
+            mkdir -p -v "$EXTRACT_DIR/$(basename "$1" $EXT)"
+            echo "Extracting $1 to $EXTRACT_DIR/$(basename "$1" $EXT)..."
+            mcopy -si "$1" ::\* "$EXTRACT_DIR/$(basename "$1" $EXT)"
+         else
+            echo "Extracting $1 to $EXTRACT_DIR..."
+            mcopy -si "$1" ::\* "$EXTRACT_DIR"
+         fi
+         ;;
+   esac
+   shift
+done
 
